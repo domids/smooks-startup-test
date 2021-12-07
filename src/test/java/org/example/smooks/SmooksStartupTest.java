32ede8f6fae91ff5295fa24b63bb59f4bd72017a
Dominik DS <seitzdo@gmail.com>
2021-12-07 09:43:51 +0100

    add test

--- src/test/java/org/example/smooks/SmooksStartupTest.java
+++ src/test/java/org/example/smooks/SmooksStartupTest.java
@@ -1,12 +1,14 @@
 package org.example.smooks;
 
+import org.junit.jupiter.api.Test;
 import org.smooks.Smooks;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 
-public class Main {
-	public static void main(String[] args) throws IOException, SAXException {
+public class SmooksStartupTest {
+	@Test
+	void startupSmooks() throws IOException, SAXException {
 		final long start = System.currentTimeMillis();
 		Smooks smooks = new Smooks("smooks-config.xml");
 		System.out.println(System.currentTimeMillis() - start);
